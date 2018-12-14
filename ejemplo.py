a66eeb191309f679bbd667ade0bd77085006907a
samhv <usam92@gmail.com>
2018-12-14 23:59:44 +0000

    add: init

---
+++ ejemplo.py
@@ -0,0 +1,47 @@
+def oscar_carrasco():
+    # esto lo editara Oscar Carrasco
+    # esta funcion debe retornar 10 para estar lista
+    a = 1
+    b = 2
+    return a + b
+
+def jose_gregorio():
+    # esto lo editara Jose Gregorio
+    # esta funcion debe retornar 18 para estar lista
+    c = 1
+    d = 2
+    return c + d
+
+def jose_navarro():
+    # esto lo editara Jose Gregorio
+    # esta funcion debe retornar 5 para estar lista
+    e = 1
+    f = 2
+    return e + f
+
+def andy_torres():
+    # esta funcion debe retornar 25 para estar lista
+    g = 1
+    h = 2
+    return g + h
+
+if oscar_carrasco() == 10:
+    print('oscar listo :D')
+else:
+    print('edita tu funcion oscar :(')
+
+if jose_gregorio() == 18:
+    print('jose gregorio listo :D')
+else:
+    print('edita tu funcion jose gregorio :(')
+
+if jose_navarro() == 5:
+    print('jose_navarro listo :D')
+else:
+    print('edita tu funcion jose_navarro :(')
+
+if andy_torres() == 25:
+    print('andy_torres listo :D')
+else:
+    print('edita tu funcion andy_torres :(')
+    
